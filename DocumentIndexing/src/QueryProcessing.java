1eae87b42f3dfc63d138d54562d204662c293eea
graciasrochelle <graciasrochelle@gmail.com>
2018-04-02 15:16:07 +1000

    bug fix

--- DocumentIndexing/src/QueryProcessing.java
+++ DocumentIndexing/src/QueryProcessing.java
@@ -12,35 +12,34 @@ class QueryProcessing {
 		stopwordRemover = new StopwordRemover("/home/inforet/a1/stoplist");
 		parse();
 	}
-	
+
 	private void parse() {
 		filteringQuery();
-        toLowerCase();
-        tokenise();
-        removeStopwords();
-    }
+		toLowerCase();
+		tokenise();
+		removeStopwords();
+	}
 
 	private void filteringQuery() {
-		query = query.replaceAll("[\\p{Punct}]", " ").replaceAll("[0-9]", "");
+		query = query.replaceAll("[\\p{Punct}]", " ").replaceAll("\\d+", "");
 	}
 
 	private void toLowerCase() {
 		query = query.toLowerCase();
 	}
-	
-    private void tokenise() {
-        String[] splitText = query.split("\\s+");
-        queryTerms = new ArrayList<>(Arrays.asList(splitText));
-    }
+
+	private void tokenise() {
+		String[] splitText = query.split("\\s+");
+		queryTerms = new ArrayList<>(Arrays.asList(splitText));
+	}
 
 	private void removeStopwords() {
 		if (stopwordRemover != null) {
 			queryTerms = stopwordRemover.removeStopwords(queryTerms);
-        }
+		}
 	}
 
 	ArrayList<String> getQueryTerms() {
 		return queryTerms;
 	}
-
 }
--- DocumentIndexing/src/SearchEngine.java
+++ DocumentIndexing/src/SearchEngine.java
@@ -4,113 +4,115 @@ import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-class SearchEngine {
-    private static final String READ_MODE = "r";
-    private static final boolean VERBOSE = false;
+public class SearchEngine {
 
-    private final HashMap<String, IndexEntry> lexicon = new HashMap<>();
-    private final HashMap<Integer, String> documentIDMap = new HashMap<>();
-    private final String indexFilename;
+	private static final String READ_MODE = "r";
+	private static final boolean VERBOSE = false;
 
-    SearchEngine(String lexiconFilename, String indexFilename, String mapFilename) {
-        readMapFile(mapFilename);
-        readLexiconFile(lexiconFilename);
-        this.indexFilename = indexFilename;
-    }
+	private HashMap<String, IndexEntry> lexicon = new HashMap<>();
+	private HashMap<Integer, String> documentIDMap = new HashMap<>();
+	private String indexFilename;
 
-    private void readMapFile(String mapFilename) {
-        try (RandomAccessFile mapFile = new RandomAccessFile(mapFilename, READ_MODE)) {
-            long endOfFile = mapFile.length();
 
-            while (mapFile.getFilePointer() < endOfFile) {
-                int docIndex = mapFile.readInt();
-                String docNo = mapFile.readUTF();
-                documentIDMap.put(docIndex, docNo);
-            }
-        } catch (EOFException e) {
-            System.err.println("You reached the end of the map file!");
-        } catch (IOException e) {
-            System.err.println("Oops! Problem with reading from map file!");
-            e.printStackTrace();
-        }
-    }
+	public SearchEngine(String lexiconFilename, String indexFilename, String mapFilename) {
+		readMapFile(mapFilename);
+		readLexiconFile(lexiconFilename);
+		this.indexFilename = indexFilename;
+	}
 
-    private void readLexiconFile(String lexiconFilename) {
-        try (RandomAccessFile lexiconFile = new RandomAccessFile(lexiconFilename, READ_MODE)) {
-            long endOfFile = lexiconFile.length();
-            long startOfFile = 0L;
+	private void readMapFile(String mapFilename) {
+		try (RandomAccessFile mapFile = new RandomAccessFile(mapFilename, READ_MODE)) {
+			long endOfFile = mapFile.length();
 
-            lexiconFile.seek(startOfFile);
+			while (mapFile.getFilePointer() < endOfFile) {
+				int docIndex = mapFile.readInt();
+				String docNo = mapFile.readUTF();
+				documentIDMap.put(docIndex, docNo);
+			}
+		} catch (EOFException e) {
+			System.err.println("You read the map file!");
+		} catch (IOException e) {
+			System.err.println("Oops! Problem with reading from map file!");
+			e.printStackTrace();
+		}
+	}
 
-            while (lexiconFile.getFilePointer() <= endOfFile) {
-                IndexEntry entry = new IndexEntry();
-                String term = lexiconFile.readUTF();
-                int docFrequency = lexiconFile.readInt();
-                long byteOffset = lexiconFile.readLong();
-                entry.setTerm(term);
-                entry.setDocumentFrequency(docFrequency);
-                entry.setByteOffset(byteOffset);
+	private void readLexiconFile(String lexiconFilename) {
+		try (RandomAccessFile lexiconFile = new RandomAccessFile(lexiconFilename, READ_MODE)) {
+			long endOfFile = lexiconFile.length();
+			long startOfFile = 0L;
 
-                if (VERBOSE) {
-                    System.out.print("term: " + term);
-                    System.out.print("||docFrequency:" + docFrequency);
-                    System.out.print("||byteOffset:" + byteOffset);
-                    System.out.println();
-                }
+			lexiconFile.seek(startOfFile);
 
-                lexicon.put(term, entry);
-            }
-        } catch (EOFException e) {
-            System.err.println("You reached the end of the lexicon file!\n");
-        } catch (IOException e) {
-            System.err.println("Problem with reading from lexicon file!\n");
-            e.printStackTrace();
-        }
-    }
-    
-    void processQueryTerms(String query){
+			while (lexiconFile.getFilePointer() <= endOfFile) {
+				IndexEntry entry = new IndexEntry();
+				String term = lexiconFile.readUTF();
+				int docFrequency = lexiconFile.readInt();
+				long byteOffset = lexiconFile.readLong();
+				entry.setTerm(term);
+				entry.setDocumentFrequency(docFrequency);
+				entry.setByteOffset(byteOffset);
+
+				if (VERBOSE) {
+					System.out.print("term: " + term);
+					System.out.print("||docFrequency:" + docFrequency);
+					System.out.print("||byteOffset:" + byteOffset);
+					System.out.println();
+				}
+
+				lexicon.put(term, entry);
+			}
+		} catch (EOFException e) {
+			System.err.println("You read the lexicon file!\n");
+		} catch (IOException e) {
+			System.err.println("Problem with reading from lexicon file!\n");
+			e.printStackTrace();
+		}
+	}
+
+	public void processQueryTerms(String query){
 		QueryProcessing queryProcessor = new QueryProcessing(query);
 		ArrayList<String> queryTerm = queryProcessor.getQueryTerms();
-        for (String aQueryTerm : queryTerm) {
-            System.out.println(search(aQueryTerm));
-        }
-    }
+		for(int i=0; i<queryTerm.size(); i++){
+			System.out.println(search(queryTerm.get(i)));
+		}
+	}
 
-    private String search(String query) {
-        if (lexicon.containsKey(query)) {
-            StringBuilder queryResult = new StringBuilder();
-            queryResult.append(query).append("\n");
+	public String search(String query) {
+		if (!query.equals("") && lexicon.containsKey(query)) {
+			StringBuilder queryResult = new StringBuilder();
+			queryResult.append(query).append("\n");
 
-            IndexEntry entry = lexicon.get(query);
+			IndexEntry entry = lexicon.get(query);
 
-            int docFrequency = entry.getDocumentFrequency();
-            queryResult.append(docFrequency).append("\n");
+			int docFrequency = entry.getDocumentFrequency();
+			queryResult.append(docFrequency).append("\n");
 
-            try (RandomAccessFile indexFile = new RandomAccessFile(indexFilename, READ_MODE)) {
-                indexFile.seek(entry.getByteOffset());
+			try (RandomAccessFile indexFile = new RandomAccessFile(indexFilename, READ_MODE)) {
+				indexFile.seek(entry.getByteOffset());
 
-                for (int i = 0; i < docFrequency; i++) {
-                    int docID = indexFile.readInt();
-                    int termFrequency = indexFile.readInt();
-                    String docNo = documentIDMap.get(docID);
+				for (int i = 0; i < docFrequency; i++) {
+					int docID = indexFile.readInt();
+					int termFrequency = indexFile.readInt();
+					String docNo = documentIDMap.get(docID);
 
-                    if (VERBOSE) {
-                        System.out.print("DocID: " + docID);
-                        System.out.print("||DocNo:" + docNo);
-                        System.out.print("||termFrequency:" + termFrequency);
-                        System.out.println();
-                    }
+					if (VERBOSE) {
+						System.out.print("DocID: " + docID);
+						System.out.print("||DocNo:" + docNo);
+						System.out.print("||termFrequency:" + termFrequency);
+						System.out.println();
+					}
 
-                    queryResult.append(docNo).append(" ").append(termFrequency).append("\n");
-                }
+					queryResult.append(docNo).append(" ").append(termFrequency).append("\n");
+				}
 
-            } catch (IOException e) {
-                System.err.println("Problem with reading from index file!\n");
-                e.printStackTrace();
-            }
-            return queryResult.toString();
-        } else {
-            return "\nSearch term:" + query + " not found!";
-        }
-    }
+			} catch (IOException e) {
+				System.err.println("Problem with reading from index file!\n");
+				e.printStackTrace();
+			}
+			return queryResult.toString();
+		} else {
+			return "\nSearch term:"+ query +" not found!";
+		}
+	}
 }
